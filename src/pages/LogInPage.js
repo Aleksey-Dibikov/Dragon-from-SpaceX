23d46ead4e5eafcd22929792a8187ca1bada747a
AlekseyDibikov <79219724+Aleksey-Dibikov@users.noreply.github.com>
2022-10-03 01:05:43 +0300

    style

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -1,12 +1,16 @@
+import Logo from '../img/DragonLogo.png';
 import LogIn from '../components/LogIn';
 import s from '../style/FormRegister.module.css';
 
 function LogInPage() {
   return (
-    <div className={s.containerForm}>
+    <>
+      <img src={Logo} alt="dragon logo" className={s.imgLogo} />
+     <div className={s.containerForm}>
       <h1>Login page</h1>
       <LogIn />
     </div>
+    </>
   )
 }
 
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,12 +1,16 @@
+import Logo from '../img/logo.png';
 import SignUp from '../components/SignUp';
 import s from '../style/FormRegister.module.css';
 
 function RegisterPage() {
   return (
-    <div className={s.containerForm}>
-      <h1>Registration page</h1>
-      <SignUp />
-    </div>
+    <>
+      <img src={Logo} alt="dragon logo" className={s.imgLogo} />
+      <div className={s.containerForm}>
+        <h1>Registration page</h1>
+        <SignUp />
+      </div>
+    </>
   )
 }
 
