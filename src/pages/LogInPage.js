3e6fb86ef07e4cc7f080c336724ec3e90e8ddf3f
AlekseyDibikov <79219724+Aleksey-Dibikov@users.noreply.github.com>
2022-10-21 22:32:06 +0300

    adds spinner for login and signup

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -1,10 +1,21 @@
 import Logo from '../img/DragonLogo.png';
 import LogIn from '../components/LogIn';
 import s from '../style/FormRegister.module.css';
+import { useEffect, useState } from 'react';
+import Spinner from '../components/Spinner/Spinner';
 
 function LogInPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
   return (
-    <>
+    isLoading ? <Spinner/> : <>
       <img src={Logo} alt="dragon logo" className={s.imgLogo} />
       <div className={s.containerForm}>
         <h1>Login page</h1>
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react';
 import Logo from '../img/logo.png';
 import SignUp from '../components/SignUp';
 import s from '../style/FormRegister.module.css';
+import Spinner from '../components/Spinner/Spinner';
 
 function RegisterPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
   return (
-    <>
+    isLoading ? <Spinner/> : <>
       <img src={Logo} alt="dragon logo" className={s.imgLogo} />
       <div className={s.containerForm}>
         <h1>Registration page</h1>
